bbd777058dfda96a221f88ea9fa2a0723dcacc1b
ASpace <aryan.dec25@gmail.com>
2016-09-06 22:11:49 +0530

    Add files via upload

---
+++ Voice_Activation/Voice_Activation.ino
@@ -0,0 +1,57 @@
+//By:ASpace
+//Youtube channel:https://www.youtube.com/channel/UC9i2-Fy9IOo8yzypeB7Xpow
+//Voice Activated Arduino (Bluetooth + Android)
+
+String voice;
+int  red = 3; //Connect LED 1 To Pin #3 
+int green = 4; //Connect LED 2 To Pin #4 
+int blue= 5; //Connect LED 3 To Pin #5 
+ 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(red, OUTPUT); 
+  pinMode(green, OUTPUT); 
+  pinMode(blue, OUTPUT); 
+  digitalWrite(red, HIGH); //This statement indicates that initally the Leds are off,since this a common anode configuration digitalWrite is HIGH.
+  digitalWrite(green, HIGH); 
+  digitalWrite(blue, HIGH); 
+ 
+} 
+void loop() {
+  while (Serial.available()){  //Check if there is an available byte to read
+  delay(10); //Delay added to make thing stable 
+  char c = Serial.read(); //Conduct a serial read
+  if (c == '#') {break;} //Exit the loop when the # is detected after the word
+  voice += c; //Shorthand for voice = voice + c
+  }  
+  if (voice.length() > 0) {
+    Serial.println(voice); 
+
+  if(voice == "*all on") {allon();}  //Turn Off All Pins (Call Function)
+  else if(voice == "*all off"){alloff();} //Turn On  All Pins (Call Function)
+  
+
+  else if(voice == "*red on") {digitalWrite(red, LOW);} 
+  else if(voice == "*green on") {digitalWrite(green, LOW);}
+  else if(voice == "*blue on") {digitalWrite(blue, LOW);}
+  
+  else if(voice == "*red") {digitalWrite(red, HIGH);} 
+  else if(voice == "*green") {digitalWrite(green, HIGH);}
+  else if(voice == "*blue") {digitalWrite(blue, HIGH);}
+   
+voice="";}} //Reset the variable after initiating
+
+void allon()
+{
+     digitalWrite(red, LOW); 
+     digitalWrite(green, LOW); 
+     digitalWrite(blue, LOW);
+     
+}
+void alloff()
+{
+     digitalWrite(red, HIGH); 
+     digitalWrite(green, HIGH); 
+     digitalWrite(blue, HIGH); 
+}
